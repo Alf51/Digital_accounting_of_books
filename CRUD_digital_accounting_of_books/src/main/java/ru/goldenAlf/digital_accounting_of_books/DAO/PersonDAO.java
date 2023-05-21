2284fbe9911c2ab34ff2a9f2f0e0a61bc1532b1a
Vasiliy <zakupm51@gmail.com>
2023-05-21 20:32:41 +0300

    add PersonMapper

--- CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/DAO/PersonDAO.java
+++ CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/DAO/PersonDAO.java
@@ -1,19 +1,32 @@
 package ru.goldenAlf.digital_accounting_of_books.DAO;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.goldenAlf.digital_accounting_of_books.model.Person;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     //TODO: Список всех людей
-    public Person[] index(){
+    public List<Person> index() {
+        jdbcTemplate.query("SELECT * From person", new BeanPropertyRowMapper<>());
         return null;
     }
 
     //TODO: Возвращает конкретного человека
-    public Person show() {
-        return null;
+    public Optional<Person> show() {
+        return Optional.empty();
     }
 
     public void save(Person person) {
