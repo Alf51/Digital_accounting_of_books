6273ea89f40835d824acffd6741daf9f4c1b9d0e
Vasiliy <zakupm51@gmail.com>
2023-05-21 20:59:29 +0300

    completed PersonDAO

--- CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/DAO/PersonDAO.java
+++ CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/DAO/PersonDAO.java
@@ -1,10 +1,10 @@
 package ru.goldenAlf.digital_accounting_of_books.DAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.goldenAlf.digital_accounting_of_books.model.Person;
+import ru.goldenAlf.digital_accounting_of_books.utils.PersonMapper;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,26 +18,31 @@ public class PersonDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    //TODO: Список всех людей
+    //Список всех людей
     public List<Person> index() {
-        jdbcTemplate.query("SELECT * From person", new BeanPropertyRowMapper<>());
+        jdbcTemplate.query("SELECT * From person", new PersonMapper());
         return null;
     }
 
-    //TODO: Возвращает конкретного человека
-    public Optional<Person> show() {
-        return Optional.empty();
+    //Возвращает конкретного человека
+    public Optional<Person> show(int id) {
+        return jdbcTemplate.query("SELECT * FROM person WHERE id=?",
+                new PersonMapper(),
+                id).stream().findAny();
     }
 
-    public void save(Person person) {
-
+    public void save(int id, Person personUpdate) {
+        jdbcTemplate.query("INSERT INTO person(id, fullname, birthday) VALUES (?, ?, ?)", new PersonMapper());
     }
 
     public void delete(int id) {
-
+        jdbcTemplate.query("DELETE FROM person WHERE id=?", new PersonMapper(), id);
     }
 
-    public void update(Person person) {
-
+    public void update(int id, Person personUpdate) {
+        jdbcTemplate.query("UPDATE person SET fullname=?, birthday=? WHERE id=?", new PersonMapper(),
+                personUpdate.getFullName(),
+                personUpdate.getBirthday(),
+                id);
     }
 }
