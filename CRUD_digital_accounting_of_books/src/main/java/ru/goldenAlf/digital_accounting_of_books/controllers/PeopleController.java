72f1ea05f1bd944a5bebb25c55644e22bc66407b
Vasiliy <zakupm51@gmail.com>
2023-05-29 18:14:10 +0300

    add PostMapping

--- CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/controllers/PeopleController.java
+++ CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/controllers/PeopleController.java
@@ -1,11 +1,16 @@
 package ru.goldenAlf.digital_accounting_of_books.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.goldenAlf.digital_accounting_of_books.DAO.PersonDAO;
+import ru.goldenAlf.digital_accounting_of_books.model.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -21,5 +26,20 @@ public class PeopleController {
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
+    }
+
+    @GetMapping({"/new", "/new/"})
+    public String newPersonCreate(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String save (@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
