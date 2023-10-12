096ee80a94a0c21d297ae1beeda0e5f05811a3d9
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-12 21:53:51 +0300

    .gitignore

--- CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/utils/PersonMapper.java
+++ CRUD_digital_accounting_of_books/src/main/java/ru/goldenAlf/digital_accounting_of_books/utils/PersonMapper.java
@@ -2,12 +2,8 @@ package ru.goldenAlf.digital_accounting_of_books.utils;
 
 import org.springframework.jdbc.core.RowMapper;
 import ru.goldenAlf.digital_accounting_of_books.model.Person;
-
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 
 public class PersonMapper implements RowMapper<Person> {
     @Override
@@ -16,8 +12,6 @@ public class PersonMapper implements RowMapper<Person> {
         person.setId(resultSet.getInt("id"));
         person.setFullName(resultSet.getString("fullname"));
 
-//        Date date = resultSet.getDate("birthday");
-//        LocalDate localDateTime = LocalDate.parse(new SimpleDateFormat("yyyy-MM-dd").format(date));
         person.setBirthday(resultSet.getString("birthday"));
         return person;
     }
