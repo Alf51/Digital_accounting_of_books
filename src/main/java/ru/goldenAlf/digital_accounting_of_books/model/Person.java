3b77d12a988abb8f8f3ad5b5b175f91d149cfa97
Vasiliy Vozniuk <zakupx51@yandex.ru>
2023-10-13 22:55:50 +0300

    Book and Person class make @Entity

--- src/main/java/ru/goldenAlf/digital_accounting_of_books/model/Person.java
+++ src/main/java/ru/goldenAlf/digital_accounting_of_books/model/Person.java
@@ -1,16 +1,29 @@
 package ru.goldenAlf.digital_accounting_of_books.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
+import java.util.List;
+
+@Entity
+@Table(name = "person")
 public class Person {
+    @Id
+    @Column(name = "id")
     private int id;
 
     @NotEmpty(message = "Полное имя не может быть пустым")
     @Pattern(regexp = "[А-Яа-я]*?\\s[А-Яа-я]*?\\s[А-Яа-я]*", message = "Напишите польностью ФИО")
+    @Column(name = "fullname")
     private String fullName;
+
     @Pattern(regexp = "(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d", message = "Неправельный формат даты, введи дату dd.MM.yyyy")
+    @Column(name = "birthday")
     private String birthday;
+
+    @OneToMany(mappedBy = "person")
+    private List<Book> bookList;
 
     // Конструктор по умолчанию нужен для Spring
     public Person() {
@@ -44,5 +57,13 @@ public class Person {
 
     public void setBirthday(String birthday) {
         this.birthday = birthday;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
     }
 }
